b968fb9493b92bd02cc0903a77b7dd3f0e763687
Cristian <lcristianiim@yahoo.com>
2017-02-24 16:58:58 +0200

    Fixing route bug

--- categories/items/edit/item-edit.js
+++ categories/items/edit/item-edit.js
@@ -4,9 +4,9 @@ angular.module("categories.items.edit", [
     .config(function($stateProvider){
         $stateProvider
             .state("ticketer.categories.items.edit", {
-                url: ":itemCategory/items/:itemId/edit",
-                        templateUrl: "categories/items/edit/item-edit.tmpl.html",
-                        controller: "EditItemsController as editItemsController"
+                url: "/items/:itemId/edit",
+                templateUrl: "categories/items/edit/item-edit.tmpl.html",
+                controller: "EditItemsController as editItemsController"
             })
     })
     .controller("EditItemsController", function($q, $state, $stateParams, ItemsModel, CategoriesModel) {
